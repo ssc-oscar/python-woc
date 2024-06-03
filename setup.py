b35eea5c23c56190f29f21e700e593bff5834177
12f23eddde <12f23eddde@gmail.com>
2024-06-04 02:05:27 +0800

    chore: enable line profiling

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+# distutils: define_macros=CYTHON_TRACE_NOGIL=1
+
 import os
 from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
+
+# Thanks to @tryptofame for proposing an updated snippet
+from Cython.Compiler.Options import get_directive_defaults
+directive_defaults = get_directive_defaults()
+
+directive_defaults['linetrace'] = True
+directive_defaults['binding'] = True
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 PACKAGE_ROOT = os.path.join(ROOT, "woc")
@@ -15,7 +24,8 @@ ext_modules = [
         sources=[
             os.path.join(PACKAGE_ROOT, 'local.pyx'),
         ], 
-        extra_compile_args=['-std=gnu11']
+        extra_compile_args=['-std=gnu11'],
+        define_macros=[('CYTHON_TRACE', '1')],
     ),
     Extension(
         'woc.tch', 
@@ -28,13 +38,14 @@ ext_modules = [
             os.path.join(TCH_ROOT, 'tcutil.c'),
             os.path.join(TCH_ROOT, 'md5.c')
         ], 
-        extra_compile_args=['-std=gnu11']
+        extra_compile_args=['-std=gnu11'],
+        define_macros=[('CYTHON_TRACE', '1')],
     ),
 ]
 
 setup(
     name="python-woc",
-    ext_modules=cythonize(ext_modules),
+    ext_modules=cythonize(ext_modules, emit_linenums=True,),
     packages=PACKAGES,
     package_data={"": ["*.pyx", "*.pxd", "*.pxi"]},
     include_package_data=True,
